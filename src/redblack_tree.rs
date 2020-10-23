e309dadda65b59de5bc8cd1a198052a42d7fa14b
ikanago <28985004+ikanago@users.noreply.github.com>
2020-10-23 17:24:55 +0900

    feat: Initialize root node

--- src/redblack_tree.rs
+++ src/redblack_tree.rs
@@ -7,11 +7,26 @@ impl<T: Ord> RedBlackTree<T> {
     pub fn new() -> Self {
         Self { root: None }
     }
+
+    pub fn insert(&mut self, key: T) {
+        match &self.root {
+            Some(_) => unimplemented!(),
+            None => {
+                self.root = Some(Node::new(key));
+            }
+        }
+    }
 }
 
 #[derive(Debug, PartialEq)]
 struct Node<T: Ord> {
     key: T,
+}
+
+impl<T: Ord> Node<T> {
+    fn new(key: T) -> Self {
+        Self { key }
+    }
 }
 
 #[cfg(test)]
@@ -20,5 +35,17 @@ mod tests {
     #[test]
     fn initilization() {
         assert_eq!(RedBlackTree { root: None }, RedBlackTree::<u64>::new());
+    }
+
+    #[test]
+    fn insert_root() {
+        let mut tree = RedBlackTree::new();
+        tree.insert(42);
+        assert_eq!(
+            RedBlackTree {
+                root: Some(Node::new(42))
+            },
+            tree
+        );
     }
 }
