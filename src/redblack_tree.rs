39260907cd57ccc2b633d13bda17764a2182abb9
ikanago <28985004+ikanago@users.noreply.github.com>
2020-10-24 20:05:45 +0900

    feat: Implement left rotation

--- src/redblack_tree.rs
+++ src/redblack_tree.rs
@@ -1,6 +1,9 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+
 #[derive(Debug, PartialEq)]
 pub struct RedBlackTree<T: Ord> {
-    root: Option<Node<T>>,
+    root: Option<Rc<Node<T>>>,
 }
 
 impl<T: Ord> RedBlackTree<T> {
@@ -12,7 +15,7 @@ impl<T: Ord> RedBlackTree<T> {
         match &self.root {
             Some(_) => unimplemented!(),
             None => {
-                self.root = Some(Node::new(key));
+                self.root = Some(Rc::new(Node::new(key, None, None)));
             }
         }
     }
@@ -21,11 +24,32 @@ impl<T: Ord> RedBlackTree<T> {
 #[derive(Debug, PartialEq)]
 struct Node<T: Ord> {
     key: T,
+    lhs: RefCell<Option<Rc<Node<T>>>>,
+    rhs: RefCell<Option<Rc<Node<T>>>>,
 }
 
 impl<T: Ord> Node<T> {
-    fn new(key: T) -> Self {
-        Self { key }
+    fn new(key: T, lhs: Option<Node<T>>, rhs: Option<Node<T>>) -> Self {
+        Self {
+            key,
+            lhs: RefCell::new(lhs.map(|node| Rc::new(node))),
+            rhs: RefCell::new(rhs.map(|node| Rc::new(node))),
+        }
+    }
+
+    fn rotate_left(node: Rc<Node<T>>) -> Rc<Node<T>> {
+        if node.rhs.borrow().is_none() {
+            return node;
+        }
+
+        let new_rotation_root = Rc::clone(node.rhs.borrow().as_ref().unwrap());
+        *node.rhs.borrow_mut() = new_rotation_root
+            .lhs
+            .borrow()
+            .as_ref()
+            .and_then(|node| Some(Rc::clone(node)));
+        *new_rotation_root.lhs.borrow_mut() = Some(node);
+        new_rotation_root
     }
 }
 
@@ -43,9 +67,43 @@ mod tests {
         tree.insert(42);
         assert_eq!(
             RedBlackTree {
-                root: Some(Node::new(42))
+                root: Some(Rc::new(Node {
+                    key: 42,
+                    lhs: RefCell::new(None),
+                    rhs: RefCell::new(None)
+                }))
             },
             tree
         );
+    }
+
+    #[test]
+    fn left_rotation() {
+        let mut tree = RedBlackTree {
+            root: Some(Rc::new(Node::new(
+                11,
+                Some(Node::new(9, None, None)),
+                Some(Node::new(
+                    18,
+                    Some(Node::new(14, None, None)),
+                    Some(Node::new(19, None, None)),
+                )),
+            ))),
+        };
+        tree.root = Some(Node::rotate_left(tree.root.unwrap()));
+        assert_eq!(
+            RedBlackTree {
+                root: Some(Rc::new(Node::new(
+                    18,
+                    Some(Node::new(
+                        11,
+                        Some(Node::new(9, None, None)),
+                        Some(Node::new(14, None, None)),
+                    )),
+                    Some(Node::new(19, None, None)),
+                ))),
+            },
+            tree
+        )
     }
 }
